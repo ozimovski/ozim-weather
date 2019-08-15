739ed9c3331e9175b74fdfa8c62d4c670ab3fe7b
ozimovski <zan.ozim@gmail.com>
2019-08-15 18:33:54 +0200

    basic layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,16 @@
+// modules
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
+// components
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './layouts/header/header.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, HeaderComponent, HomeComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
