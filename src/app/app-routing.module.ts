6834ca8428362bfcbbbc9792c36a9eeade9acccf
ozimovski <zan.ozim@gmail.com>
2019-08-16 16:59:10 +0200

    routing fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 // components
 import { HomeComponent } from './pages/home/home.component';
 
-const routes: Routes = [{ path: '', component: HomeComponent }];
+const routes: Routes = [{ path: '', component: HomeComponent, pathMatch: 'full' }, { path: '**', redirectTo: '' }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
